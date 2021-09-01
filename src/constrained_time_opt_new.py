6bae60070ba685eb0bef831560bdb082887af11f
atharva417 <atharvamete12@gmail.com>
2021-09-02 01:02:55 +0530

    test

--- src/constrained_time_opt_new.py
+++ src/constrained_time_opt_new.py
@@ -5,7 +5,6 @@ from qpsolvers import solve_qp
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 import warnings
-import tensorflow as tf
 from scipy.optimize import Bounds,minimize
 warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
 warnings.filterwarnings("ignore", category=FutureWarning)
